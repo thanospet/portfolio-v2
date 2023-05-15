4c7cd6e2e602f2602bcc7b91083704c4cebae014
thanospet <thanospets98@gmail.com>
2023-05-16 00:39:18 +0300

    text fix

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -19,7 +19,7 @@ const FeedbackCard = ({ name, message, userIcon, userEmail }) => (
     
     <motion.div className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
       <p className="text-white font-black text-[48px]">"</p>
-      <p className="text-white font-black text-[22px]">{message}</p>
+      <p className="text-white font-black text-lg whitespace-pre-line break-words">{message}</p>
 
       <div className="mt-1">
         <p className="text-white tracking-wider text-[18px]"></p>
